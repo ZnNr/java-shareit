2ec768b43e11f46961358e151c167f740bc35d32
lightspot <zinnurshamsutdinov@yandex.ru>
2023-04-09 15:04:46 +0300

    Спринт 13 add-controllers

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,30 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.Builder;
+import ru.practicum.shareit.request.ItemRequest;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder
 public class ItemDto {
+
+    private Long id;
+
+    @NotBlank(message = "Название не может быть пустым!")
+    private String name;
+
+    @NotBlank(message = "Описание не может быть пустым!")
+    private String description;
+
+    @NotNull(message = "Статус о том дуоступен ли товар или нет не должен быть пустым!")
+    private Boolean available;
+
+    private ItemRequest request;
 }
